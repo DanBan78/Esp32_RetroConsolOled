03df5e5268136e8c0be3bb46a0ef08cd3ce34f34
Daniel Banaszek <daniel.banaszek@gmail.com>
2025-10-11 16:52:18 +0200

    striker

---
+++ RetroConsolEsp32Oled/gameStriker/Striker.h
@@ -0,0 +1,12 @@
+#ifndef STRIKER_H
+#define STRIKER_H
+
+  #include "../global/global.h"
+
+namespace StrikerGame {
+  void Game_Striker();
+}
+  
+  extern const GameInfo GameInfo_Striker;
+
+#endif
